4116babb9eb647bef8939a7fd534f0b0204474f3
zejunchen-zejun <zejun.chen@amd.com>
2026-05-11 20:33:18 +0800

    fix import issue
    Signed-off-by: zejunchen-zejun <zejun.chen@amd.com>

--- atom/plugin/vllm/attention/backend.py
+++ atom/plugin/vllm/attention/backend.py
@@ -20,10 +20,6 @@ from atom.plugin.vllm.attention.metadata import (
     AiterMLACommonPrefillMetadataForPluginMode,
     AiterMLADecodeMetadataForPluginMode,
 )
-from atom.plugin.vllm.attention_backend.mla_sparse import (
-    AiterMLASparseIndexerMetadataBuilder,
-    AiterMLASparseMetadataBuilder,
-)
 from vllm.model_executor.layers.attention.mla_attention import (
     MLACommonMetadataBuilder,
     QueryLenSupport,
@@ -1071,6 +1067,10 @@ class AiterSparseMlaBackendForVllm(AiterMlaBackendForVllm):
 
     @staticmethod
     def get_builder_cls() -> Type["AiterMLASparseMetadataBuilder"]:
+        from atom.plugin.vllm.attention_backend.mla_sparse import (
+            AiterMLASparseMetadataBuilder,
+        )
+
         return AiterMLASparseMetadataBuilder
 
     @classmethod
@@ -1093,6 +1093,10 @@ class AiterSparseMlaIndexerBackendForVllm(AiterMlaBackendForVllm):
 
     @staticmethod
     def get_builder_cls() -> Type["AiterMLASparseIndexerMetadataBuilder"]:
+        from atom.plugin.vllm.attention_backend.mla_sparse import (
+            AiterMLASparseIndexerMetadataBuilder,
+        )
+
         return AiterMLASparseIndexerMetadataBuilder
 
     @classmethod
